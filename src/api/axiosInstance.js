5eae9a7cc95d760e2063c47b782f3ed8da8a55ab
Rahul <aonecreations1@gmail.com>
2025-02-25 13:47:56 +0530

    Update axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'https://car-management-api-seven.vercel.app/api',
+    baseURL: process.env.REACT_APP_API_URL || 'https://car-management-api-u93r.onrender.com/api',
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
 });
